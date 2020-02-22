7be9ab0390dc09c6a85950466a7a1a91582c1349
porosh <jamilkashem2002@gmail.com>
2020-02-22 14:28:10 +0600

    navigation complete for web

---
+++ javaScript/main.js
@@ -0,0 +1,7 @@
+// typed.js
+var typed = new Typed('.type', {
+    strings: ['UI UX Designer', 'JS Devoloper','Web App Devoloper','Full Stack Web Devoloper'],
+    typeSpeed:120,
+    loop:true,
+    backSpeed:120
+  });
