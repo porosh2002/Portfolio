af549458ddc9efa21ca1527be748f1554ef95636
porosh <jamilkashem2002@gmail.com>
2020-02-22 16:26:18 +0600

    first section complete + minified

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,7 +1,1 @@
-// typed.js
-var typed = new Typed('.type', {
-    strings: ['UI UX Designer', 'JS Devoloper','Web App Devoloper','Full Stack Web Devoloper'],
-    typeSpeed:120,
-    loop:true,
-    backSpeed:120
-  });
+var typed=new Typed(".type",{strings:["UI UX Designer","JS Devoloper","Web App Devoloper","Full Stack Web Devoloper"],typeSpeed:120,loop:!0,backSpeed:120});const all_barr=document.querySelector(".all_bars"),navigation=document.querySelector(".navigation_links");all_barr.addEventListener("click",()=>{navigation.classList.toggle("on_click_navigation")});
