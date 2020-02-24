239245ee39a824e1daeddb25017d469c5b82c99c
porosh <jamilkashem2002@gmail.com>
2020-02-24 10:00:08 +0600

    about section image problem solved

--- javaScript/main.js
+++ javaScript/main.js
@@ -3,6 +3,6 @@ about_sec_img.addEventListener('mouseenter',()=>{
     about_sec_img.style.transform = "rotate(360deg)";
     about_sec_img.style.transition = "3s";
     setTimeout(()=>{
-        about_sec_img.src = '/image/own_img_C.png' },2100);
+        about_sec_img.src = 'image/own_img_C.png' },2100);
 });
 var typed=new Typed(".type",{strings:["UI UX Designer","JS Devoloper","Web App Devoloper","Full Stack Web Devoloper"],typeSpeed:120,loop:!0,backSpeed:120});const all_barr=document.querySelector(".all_bars"),navigation=document.querySelector(".navigation_links");all_barr.addEventListener("click",()=>{navigation.classList.toggle("on_click_navigation")});
