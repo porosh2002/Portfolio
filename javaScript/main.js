97cfb5aeeb3611080acbd7c7c47d0832d73bf142
porosh <jamilkashem2002@gmail.com>
2020-02-27 16:11:44 +0600

    redesign

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,8 +1,12 @@
-const about_sec_img =document.querySelector('.about_sec_img');
-about_sec_img.addEventListener('mouseenter',()=>{
-    about_sec_img.style.transform = "rotate(360deg)";
-    about_sec_img.style.transition = "3s";
-    setTimeout(()=>{
-        about_sec_img.src = 'image/own_img_C.png' },2100);
+
+// var typed = new Typed(".abut_des", {
+//     strings: ["UI UX Designer", "JS Devoloper","MD Jamil Kashem Porosh","Web App Devoloper", "Full Stack Web Devoloper"],
+//     typeSpeed: 120,
+//     loop:true,
+//     backSpeed: 120
+// });
+const all_barr = document.querySelector(".all_bars"),
+    navigation = document.querySelector(".navigation_links");
+all_barr.addEventListener("click", () => {
+    navigation.classList.toggle("on_click_navigation")
 });
-var typed=new Typed(".type",{strings:["UI UX Designer","JS Devoloper","Web App Devoloper","Full Stack Web Devoloper"],typeSpeed:120,loop:!0,backSpeed:120});const all_barr=document.querySelector(".all_bars"),navigation=document.querySelector(".navigation_links");all_barr.addEventListener("click",()=>{navigation.classList.toggle("on_click_navigation")});
