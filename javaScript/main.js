6c098a11267c912ed977598ec7bf15ed56b74ea1
porosh <jamilkashem2002@gmail.com>
2020-02-29 14:43:46 +0600

    section title added

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,9 +1,10 @@
-// var typed = new Typed(".type", {
-//     strings: ['I Love Coding so Much'],
-//     typeSpeed: 120,
-//     loop:true,
-//     backSpeed: 120
-// });
+var typed = new Typed(".type", {
+    strings: ['I Love Coding so Much','Welcome To My Website','am Porosh','am a JS Developer',' am a Full Stack Web-Devoloper'],
+    typeSpeed: 120,
+    loop:true,
+    backSpeed: 120,
+    shuffle:true,
+});
 window.addEventListener('scroll',() =>{
     const nav = document.querySelector('.wth_out_wlcm');
     const scroll_value = window.scrollY;
